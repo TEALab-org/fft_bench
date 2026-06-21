e69fbb5559a0bfc69242975a0b6b74259fa5c0f4
Russell Bentley <rbentley@login01.expanse.sdsc.edu>
2026-06-21 12:34:23 -0700

    Lots of new data

--- python_scripts/test_rust_expanse_3.py
+++ python_scripts/test_rust_expanse_3.py
@@ -2,6 +2,7 @@ import param_sweep
 import os
 
 exec_path = "/home/rbentley/fft_bench/rust_fft_bench/target/release/rust_fft_bench"
+cpp_exec_path = "/home/rbentley/fft_bench/cpp_fft_bench/cpp_fft_bench"
 results_dir = "/home/rbentley/fft_bench/results/expanse_3"
 sweep_config = {
     "cli": {
@@ -9,11 +10,30 @@ sweep_config = {
         "plan_size": [i for i in range(1000, 14000)],
         "plan_type": ["estimate", "measure"],
         "test_count": [12],
+    },
+    "tool": {
+        "tool": ["cpp", "rust"]
     }
 }
 
-def run_params(params_outer):
-    params = params_outer["cli"]
+def run_cpp_params(params):
+    plan_size = params["plan_size"]
+    test_count = params["test_count"]
+    threads = params["threads"]
+    output_path = f"{results_dir}/cpp_{plan_size}_{threads}.json"
+
+    if os.path.exists(output_path):
+        print(f"LOG: Already exists: {output_path}")
+        return;
+
+    command = f"{cpp_exec_path}"
+    command += f" {plan_size}"
+    command += f" {threads}"
+    command += f" {test_count}"
+    command += f" {output_path}"
+    os.system(command);
+
+def run_rust_params(params):
     plan_size = params["plan_size"]
     plan_type = params["plan_type"]
     test_count = params["test_count"]
@@ -25,7 +45,7 @@ def run_params(params_outer):
         print(f"LOG: Already exists: {output_path}")
         return;
 
-    command = f"{exec_path}"
+    command = f"{cpp_exec_path}"
     command += f" --plan-size {plan_size}"
     command += f" --plan-type {plan_type}"
     command += f" --threads {threads}"
@@ -34,6 +54,14 @@ def run_params(params_outer):
     command += f" --output {output_path}"
     os.system(command);
 
+def run_params(params_outer):
+    params = params_outer["cli"]
+    tool = params_outer["tool"]["tool"]
+    if tool == "rust":
+        run_rust_params(params)
+    elif tool == "cpp":
+        run_cpp_params(params)
+    
 def main():
     sweep_params = param_sweep.generate(sweep_config)
     print(f"LOG: |sweep_params| = {len(sweep_params)}")
--- slurm_jobs/expanse_3.sh
+++ slurm_jobs/expanse_3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name="expanse_1"
+#SBATCH --job-name="expanse_3"
 #SBATCH --partition=compute
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
@@ -7,5 +7,8 @@
 #SBATCH --account=sun127
 #SBATCH --export=ALL
 #SBATCH -t 06:30:00
+
+module add intel/19.1.3.304/6pv46so
+module add intel-mkl/2020.4.304/vg6aq26
 
 python /home/rbentley/fft_bench/python_scripts/test_rust_expanse_3.py
